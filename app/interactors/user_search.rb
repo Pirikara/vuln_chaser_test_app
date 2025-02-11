8b744bd697da6da3d4f3443738c7cedd993431ff
Pirikara <65005408+Pirikara@users.noreply.github.com>
2025-02-11 12:12:59 +0900

    dynamic query building

--- app/interactors/user_search.rb
+++ app/interactors/user_search.rb
@@ -2,7 +2,18 @@ class UserSearch
   include Interactor
 
   def call
-    query = "SELECT * FROM users WHERE name LIKE '%#{context.name}%'"
-    context.users = ActiveRecord::Base.connection.execute(query)
+    context.users = execute_dynamic_query
+  end
+
+  private
+
+  def execute_dynamic_query
+    query_method = "build_#{context.search_type}_query"
+    query = send(query_method)
+    ActiveRecord::Base.connection.send(:execute, query)
+  end
+
+  def build_name_query
+    "SELECT * FROM users WHERE name LIKE '%#{context.name}%'"
   end
 end
